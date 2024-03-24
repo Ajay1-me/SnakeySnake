0ad2a932970e858ebf57eae1366c3108c20bcaac
Ajaydeep Singh <deepajay0713@gmail.com>
2024-03-24 10:01:12 -0700

    Added justification to why Movable interface can be useful

--- app/src/main/java/com/example/snakeysnake/Movable.java
+++ app/src/main/java/com/example/snakeysnake/Movable.java
@@ -2,6 +2,10 @@ package com.example.snakeysnake;
 
 import android.view.MotionEvent;
 
+/*
+Created this if we consider future expansion where multiple classes might need a similar reset functionality,
+or to adhere to a design principle like the Interface Segregation Principle
+ */
 public interface Movable {
     void move();
     void switchHeading(MotionEvent motionEvent);
